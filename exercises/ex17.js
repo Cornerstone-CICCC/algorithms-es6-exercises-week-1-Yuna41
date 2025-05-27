c3f3fe8b815800b4fab5fb9666cac94ed6997d78
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-27 15:33:51 -0700

    exercise 17 completed

--- exercises/ex17.js
+++ exercises/ex17.js
@@ -4,3 +4,12 @@ Write an anonymous function expression that stores a function in a variable call
 laugh(3);
 Returns: hahaha!
 */
+
+const laugh = function(num){
+  let res = ""
+  for(let i=0; i<num; i++){
+    res += "ha"
+  }
+  return res + "!"
+}
+console.log(laugh(3))
