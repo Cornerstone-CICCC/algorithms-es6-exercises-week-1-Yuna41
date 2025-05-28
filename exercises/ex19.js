561f57a57a01ba50e42a94a64a3dad657901673a
Yuna Shimonosono <uncnprs@gmail.com>
2025-05-27 17:25:44 -0700

    exercise 19 completed

--- exercises/ex19.js
+++ exercises/ex19.js
@@ -4,3 +4,16 @@ Call the emotions() function so that it prints the output you see below, but ins
 emotions("happy", laugh(2)); // you can use your laugh function from the previous quizzes
 Prints: "I am happy, haha!"
 */
+
+
+const laugh = function(num){
+  let res = ""
+  for(let i=0; i<num; i++){
+    res += "ha"
+  }
+  return res + "!"
+}
+function emotion(myEmotion, myLaugh){
+  console.log(`I am ${myEmotion}, ${myLaugh(2)}`)
+}
+emotion("happy", laugh)
